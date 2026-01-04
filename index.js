a72f24e4adc978449610deb11f4b4c0d6bffe70e
Arthur D'Aquino <arthurdaquinosilva@gmail.com>
2026-01-04 12:28:11 -0300

    feat(express): create a basic express server

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("Initial setup");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));

34b1fdcdd0397cf5266a20de4b9e9c8733551236
Arthur D'Aquino <arthurdaquinosilva@gmail.com>
2026-01-04 13:14:09 -0300

    feat(api): add endpoint to get a single genre

--- index.js
+++ index.js
@@ -11,5 +11,16 @@ app.get("/api/genres", (request, response) => {
   response.status(200).send(genres);
 });
 
+app.get("/api/genres/:id", (request, response) => {
+  const genre = genres.find(
+    (genre) => genre.id === parseInt(request.params.id),
+  );
+  if (!genre)
+    return response
+      .status(404)
+      .send("The genre with the given id was not found.");
+  response.status(200).send(genre);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));

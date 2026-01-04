dd315d85f787a97d6b8518fee94dd4b224f09a11
Arthur D'Aquino <arthurdaquinosilva@gmail.com>
2026-01-04 12:34:55 -0300

    feat(api-genres): add endpoint to get a list of genres

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 const express = require("express");
 const app = express();
+
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Romance" },
+  { id: 3, name: "Commedy" },
+];
+
+app.get("/api/genres", (request, response) => {
+  response.status(200).send(genres);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
